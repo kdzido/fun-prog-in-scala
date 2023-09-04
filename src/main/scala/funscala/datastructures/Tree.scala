a8cc7a01c87a3e7c9a13e6b4fbc031954dbf9d26
kdzido <krzysztof.dzido@gmail.com>
2023-09-04 10:57:18 +0200

    [CHAP-3][EXERCISE-26] implement maximum on Tree

--- src/main/scala/funscala/datastructures/Tree.scala
+++ src/main/scala/funscala/datastructures/Tree.scala
@@ -12,4 +12,12 @@ object Tree {
     case Leaf(_) ⇒ 1
     case Branch(t1, t2) ⇒ 1 + size(t1) + size(t2)
   }
+
+  /** [CHAP-3][EXERCISE-26] implement maximum on Tree.
+   * NOTE non-tail recursive, for balanced trees shall not be much of issue due to O(log n) */
+  def maximum(t: Tree[Int]): Int = t match {
+    case Leaf(x) ⇒ x
+    case Branch(t1, t2) ⇒ maximum(t1) max maximum(t2)
+  }
+
 }
--- src/test/scala/funscala/datastructures/TreeTest.scala
+++ src/test/scala/funscala/datastructures/TreeTest.scala
@@ -14,5 +14,24 @@ class TreeTest extends AnyFlatSpec {
     assert(Tree.size(t5) == 5)
   }
 
+  /* [CHAP-3][EXERCISE-26] implement maximum on Tree. */
+  "Non-empty Tree of Ints" should "have maximum element" in {
+    val t1 = Leaf(1)
+    val t3a = Branch(Leaf(1), Leaf(2))
+    val t3b = Branch(Leaf(1), Leaf(2))
+
+    val t5a = Branch(Leaf(1), Branch(Leaf(5), Leaf(3)))
+    val t5b = Branch(Leaf(1), Branch(Leaf(3), Leaf(5)))
+    val t5c = Branch(Leaf(5), Branch(Leaf(1), Leaf(3)))
+
+    assert(Tree.maximum(t1) == 1)
+
+    assert(Tree.maximum(t3a) == 2)
+    assert(Tree.maximum(t3b) == 2)
+
+    assert(Tree.maximum(t5a) == 5)
+    assert(Tree.maximum(t5b) == 5)
+    assert(Tree.maximum(t5c) == 5)
+  }
 
 }
