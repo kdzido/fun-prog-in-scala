d73ccbc6b1043720ffa0724a4af6d047f98248a8
kdzido <krzysztof.dzido@gmail.com>
2023-09-04 12:12:12 +0200

    [CHAP-3][EXERCISE-29] generalize size, maximum, depth, map

--- src/main/scala/funscala/datastructures/Tree.scala
+++ src/main/scala/funscala/datastructures/Tree.scala
@@ -13,12 +13,18 @@ object Tree {
     case Branch(t1, t2) ⇒ 1 + size(t1) + size(t2)
   }
 
+  /** [CHAP-3][EXERCISE-29] generalize size, maximum, depth, map */
+  def size2[A](t: Tree[A]): Int = fold(t)(_ ⇒ 1)((ts1, ts2) ⇒ 1 + (ts1 + ts2))
+
   /** [CHAP-3][EXERCISE-26] implement maximum on Tree.
    * NOTE non-tail recursive, for balanced trees shall not be much of issue due to O(log n) */
   def maximum(t: Tree[Int]): Int = t match {
     case Leaf(x) ⇒ x
     case Branch(t1, t2) ⇒ maximum(t1) max maximum(t2)
   }
+
+  /** [CHAP-3][EXERCISE-29] generalize size, maximum, depth, map */
+  def maximum2(t: Tree[Int]): Int = fold(t)(_.value)(_ max _)
 
   /** [CHAP-3][EXERCISE-27] implement depth on Tree.
    * NOTE non-tail recursive, for balanced trees shall not be much of issue due to O(log n) */
@@ -27,11 +33,25 @@ object Tree {
     case Branch(t1, t2) ⇒ 1 + (depth(t1) max depth(t2))
   }
 
+  /** [CHAP-3][EXERCISE-29] generalize size, maximum, depth, map */
+  def depth2(t: Tree[Int]): Int = fold(t)(_ ⇒ 1)((td1, td2) ⇒ 1 + (td1 max td2))
+
   /** [CHAP-3][EXERCISE-28] implement map on Tree.
    * NOTE non-tail recursive, for balanced trees shall not be much of issue due to O(log n) */
   def map[A](t: Tree[A])(f: A => A): Tree[A] = t match {
     case Leaf(x) ⇒ Leaf(f(x))
     case Branch(t1, t2) ⇒ Branch(map(t1)(f), map(t2)(f))
+  }
+
+  /** [CHAP-3][EXERCISE-29] generalize size, maximum, depth, map */
+  def map2[A](t: Tree[A])(f: A => A): Tree[A] = // ???
+    fold(t)(l ⇒ Leaf(f(l.value)).asInstanceOf[Tree[A]])((mt1, mt2) ⇒ Branch(mt1, mt2))
+
+  /** [CHAP-3][EXERCISE-29] generalize size, maximum, depth, map
+   * NOTE non-tail recursive, for balanced trees shall not be much of issue due to O(log n) */
+  def fold[A, B](t: Tree[A])(f: Leaf[A] ⇒ B)(g: (B, B) => B): B = t match {
+    case ll: Leaf[A] ⇒ f(ll)
+    case Branch(t1, t2) ⇒ g(fold(t1)(f)(g), fold(t2)(f)(g))
   }
 
 
--- src/test/scala/funscala/datastructures/TreeTest.scala
+++ src/test/scala/funscala/datastructures/TreeTest.scala
@@ -4,7 +4,8 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 class TreeTest extends AnyFlatSpec {
 
-  /** [CHAP-3][EXERCISE-25] implement size on Tree */
+  // [CHAP-3][EXERCISE-25] implement size on Tree
+  // [CHAP-3][EXERCISE-29] generalize size, maximum, depth, map
   "Non-empty Tree of Ints" should "have size" in {
     val t1 = Leaf(1)
     val t3 = Branch(Leaf(1), Leaf(2))
@@ -12,9 +13,14 @@ class TreeTest extends AnyFlatSpec {
     assert(Tree.size(t1) == 1)
     assert(Tree.size(t3) == 3)
     assert(Tree.size(t5) == 5)
+
+    assert(Tree.size2(t1) == 1)
+    assert(Tree.size2(t3) == 3)
+    assert(Tree.size2(t5) == 5)
   }
 
-  /* [CHAP-3][EXERCISE-26] implement maximum on Tree. */
+  // [CHAP-3][EXERCISE-26] implement maximum on Tree
+  // [CHAP-3][EXERCISE-29] generalize size, maximum, depth, map
   it should "have maximum element" in {
     val t1 = Leaf(1)
     val t3a = Branch(Leaf(1), Leaf(2))
@@ -25,16 +31,22 @@ class TreeTest extends AnyFlatSpec {
     val t5c = Branch(Leaf(5), Branch(Leaf(1), Leaf(3)))
 
     assert(Tree.maximum(t1) == 1)
-
     assert(Tree.maximum(t3a) == 2)
     assert(Tree.maximum(t3b) == 2)
-
     assert(Tree.maximum(t5a) == 5)
     assert(Tree.maximum(t5b) == 5)
     assert(Tree.maximum(t5c) == 5)
+
+    assert(Tree.maximum2(t1) == 1)
+    assert(Tree.maximum2(t3a) == 2)
+    assert(Tree.maximum2(t3b) == 2)
+    assert(Tree.maximum2(t5a) == 5)
+    assert(Tree.maximum2(t5b) == 5)
+    assert(Tree.maximum2(t5c) == 5)
   }
 
-  /* [CHAP-3][EXERCISE-27] implement depeth on Tree. */
+  // [CHAP-3][EXERCISE-27] implement depeth on Tree
+  // [CHAP-3][EXERCISE-29] generalize size, maximum, depth, map
   it should "find max depth" in {
     val t1 = Leaf(1)
     val t2a = Branch(Leaf(1), Leaf(2))
@@ -49,9 +61,17 @@ class TreeTest extends AnyFlatSpec {
     assert(Tree.depth(t3a) == 3)
     assert(Tree.depth(t3b) == 3)
     assert(Tree.depth(t4a) == 4)
+
+    assert(Tree.depth2(t1) == 1)
+    assert(Tree.depth2(t2a) == 2)
+    assert(Tree.depth2(t2b) == 2)
+    assert(Tree.depth2(t3a) == 3)
+    assert(Tree.depth2(t3b) == 3)
+    assert(Tree.depth2(t4a) == 4)
   }
 
   // [CHAP-3][EXERCISE-28] implement map on Tree
+  // [CHAP-3][EXERCISE-29] generalize size, maximum, depth, map
   it should "be mapped by incrementing each leaf" in {
     val t1 = Leaf(1)
     val t2a = Branch(Leaf(1), Leaf(2))
@@ -64,6 +84,11 @@ class TreeTest extends AnyFlatSpec {
     assert(Tree.map(t2a)(incrByOne) == Branch(Leaf(2), Leaf(3)))
     assert(Tree.map(t2b)(incrByOne) == Branch(Leaf(2), Leaf(3)))
     assert(Tree.map(t3a)(incrByOne) == Branch(Leaf(2), Branch(Leaf(6), Leaf(4))))
+
+    assert(Tree.map2(t1)(incrByOne) == Leaf(2))
+    assert(Tree.map2(t2a)(incrByOne) == Branch(Leaf(2), Leaf(3)))
+    assert(Tree.map2(t2b)(incrByOne) == Branch(Leaf(2), Leaf(3)))
+    assert(Tree.map2(t3a)(incrByOne) == Branch(Leaf(2), Branch(Leaf(6), Leaf(4))))
   }
 
 }
