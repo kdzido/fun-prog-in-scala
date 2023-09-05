71b87f5900a488905eca7110c2b44eae8a89dea4
kdzido <xtofek@gmail.com>
2023-09-05 22:13:03 +0200

    [CHAP-4][EXCRCISE-01] implement Option trait

--- src/test/scala/funscala/ch4/OptionTest.scala
+++ src/test/scala/funscala/ch4/OptionTest.scala
@@ -10,4 +10,48 @@ class OptionTest extends AnyFlatSpec {
     assert(Some(1).isInstanceOf[Option[Int]])
   }
 
+  "None Option" should "always flat map to None" in {
+    assert(None.flatMap((a: Int) ⇒ Some(a.toString)) == None)
+  }
+
+  it should "always map to None" in {
+    assert(None.map((a: Int) ⇒ Some(a.toString)) == None)
+  }
+
+  it should "return getOrElse's default " in {
+    assert(None.getOrElse("<none>") == "<none>")
+  }
+
+  it should "return orElse's default " in {
+    assert(None.orElse(Some("<none>")) == Some("<none>"))
+    assert(None.orElse(None) == None)
+  }
+
+  it should "filter always to None" in {
+    assert(None.filter(_ == true) == None)
+  }
+
+  "Some Option" should "flat map to Some or None" in {
+    assert(Some(1).flatMap((a: Int) ⇒ Some(a.toString)) == Some("1"))
+    assert(Some(1).flatMap((a: Int) ⇒ None) == None)
+  }
+
+  it should "always map to Some" in {
+    assert(Some(1).map((a: Int) ⇒ a.toString) == Some("1"))
+  }
+
+  it should "never return getOrElse's default" in {
+    assert(Some(1).getOrElse("<none>") == 1)
+  }
+
+  it should "never return orElse's default " in {
+    assert(Some(1).orElse(Some("<none>")) == Some(1))
+    assert(Some(1).orElse(None) == Some(1))
+  }
+
+  it should "filter as per predicate" in {
+    assert(Some(1).filter(_ % 2 == 0) == None)
+    assert(Some(1).filter(_ % 2 == 1) == Some(1))
+  }
+
 }
