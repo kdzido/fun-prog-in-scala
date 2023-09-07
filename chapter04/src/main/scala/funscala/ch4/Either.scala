d9918733366a4f2b1c73d296a819cd830d93a649
kdzido <krzysztof.dzido@gmail.com>
2023-09-07 23:16:17 +0200

    [CHAP-4][EXERCISE-07] implement Either trait methods

--- chapter04/src/main/scala/funscala/ch4/Either.scala
+++ chapter04/src/main/scala/funscala/ch4/Either.scala
@@ -1,9 +1,34 @@
 package funscala.ch4
 
 /** Book's example */
-sealed trait Either[+E, +A]
-case class Left[+E](value: E) extends Either[E, Nothing]
-case class Right[+A](value: A) extends Either[Nothing, A]
+sealed trait Either[+E, +A] {
+  /** [CHAP-4][EXERCISE-07] implement Either trait methods */
+  def map[B](f: A ⇒ B): Either[E, B]
+  def flatMap[EE >: E, B](f: A ⇒ Either[EE, B]): Either[EE, B]
+  def orElse[EE >: E, B >: A](b: ⇒ Either[EE,B]): Either[EE,B]
+  def map2[EE >: E, B, C](b: Either[EE,B])(f: (A,B) ⇒ C): Either[EE, C]
+}
+
+/** [CHAP-4][EXERCISE-07] implement Either trait methods */
+case class Left[+E](value: E) extends Either[E, Nothing] {
+  override def map[B](f: Nothing ⇒ B): Either[E, B] = this
+  override def flatMap[EE >: E, B](f: Nothing ⇒ Either[EE, B]): Either[EE, B] = this.asInstanceOf[Either[EE,B]]
+  override def orElse[EE >: E, B >: Nothing](b: ⇒ Either[EE, B]): Either[EE, B] = b
+
+  override def map2[EE >: E, B, C](b: Either[EE, B])(f: (Nothing, B) ⇒ C): Either[EE, C] = this
+}
+
+/** [CHAP-4][EXERCISE-07] implement Either trait methods */
+case class Right[+A](value: A) extends Either[Nothing, A] {
+  override def map[B](f: A ⇒ B): Either[Nothing, B] = Right(f(value))
+  override def flatMap[EE >: Nothing, B](f: A ⇒ Either[EE, B]): Either[EE, B] = f(value)
+  override def orElse[EE >: Nothing, B >: A](b: ⇒ Either[EE, B]): Either[EE, B] = this.asInstanceOf[Either[EE,B]]
+
+  override def map2[EE >: Nothing, B, C](b: Either[EE, B])(f: (A, B) ⇒ C): Either[EE, C] = (this, b) match {
+    case (_, Left(lb)) ⇒ Left(lb)
+    case (Right(ra), Right(rb)) ⇒ Right(f(ra, rb))
+  }
+}
 
 object Either {
 
--- chapter04/src/test/scala/funscala/ch4/EitherTest.scala
+++ chapter04/src/test/scala/funscala/ch4/EitherTest.scala
@@ -15,4 +15,82 @@ class EitherTest extends AnyFlatSpec {
     assert(right.value == 1)
   }
 
+  // [CHAP-4][EXERCISE-07] implement Either trait methods
+  it should "map" in {
+    val left = Left(new IllegalArgumentException("fail!"))
+    val right = Right(1)
+
+    assertResult("fail!") {
+      left.map(_.toString) match {
+        case Left(a) ⇒ a.getMessage
+      }
+    }
+    assertResult("1") {
+      right.map(_.toString) match {
+        case Right(b) ⇒ b
+      }
+    }
+  }
+
+  it should "flatMap" in {
+    val left = Left(new IllegalArgumentException("fail!"))
+    val right = Right(1)
+
+    assertResult("fail!") {
+      left.flatMap((a) ⇒ Right(a.toString)) match {
+        case Left(a) ⇒ a.getMessage
+      }
+    }
+    assertResult("failed") {
+      right.flatMap((a) ⇒ Left("failed")) match {
+        case Left(a) ⇒ a
+      }
+    }
+    assertResult("1") {
+      right.flatMap((a) ⇒ Right(a.toString)) match {
+        case Right(b) ⇒ b
+      }
+    }
+  }
+
+  it should "orElse" in {
+    val left = Left(new IllegalArgumentException("fail!"))
+    val right = Right(1)
+
+    assertResult("orElse failed") {
+      left.orElse(Left("orElse failed")) match {
+        case Left(a) ⇒ a
+      }
+    }
+    assertResult(1) {
+      right.orElse(Left("orElse failed")) match {
+        case Right(a) ⇒ a
+      }
+    }
+  }
+
+  it should "map2" in {
+    val left = Left(new IllegalArgumentException("fail!"))
+    val left2 = Left(new IllegalArgumentException("fail2!"))
+    val right = Right(1)
+    val right2 = Right(2)
+
+    assertResult("fail!") {
+      left.map2(Right("right2"))((a,b) ⇒ a.toString) match {
+        case Left(a) ⇒ a.getMessage
+      }
+    }
+    assertResult("fail2!") {
+      right.map2(left2)((a,b) ⇒ a.toString) match {
+        case Left(a) ⇒ a.getMessage
+      }
+    }
+    assertResult("3") {
+      right.map2(right2)((a,b) ⇒ (a+b).toString) match {
+        case Right(a) ⇒ a
+      }
+    }
+  }
+
+
 }
