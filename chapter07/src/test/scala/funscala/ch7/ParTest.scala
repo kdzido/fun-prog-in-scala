8edbfd1a2fbc5658b79bcb6ccad99ea0178b57ad
kdzido <krzysztof.dzido@gmail.com>
2023-10-08 16:40:57 +0200

    [CHAP-7][BOOK-EXAMPLES] map identity laws

--- chapter07/src/test/scala/funscala/ch7/ParTest.scala
+++ chapter07/src/test/scala/funscala/ch7/ParTest.scala
@@ -1,6 +1,7 @@
 package funscala.ch7
 
 import com.google.common.util.concurrent.MoreExecutors
+import funscala.ch7.Par.{map, unit}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -12,18 +13,18 @@ class ParTest extends AnyFlatSpec with Matchers {
   val pool = Executors.newFixedThreadPool(3)
 
   it should "run unit" in {
-    Par.run(direct)(Par.unit(1)).get() shouldBe 1
-    Par.run(pool)(Par.unit(1)).get() shouldBe 1
+    Par.run(direct)(unit(1)).get() shouldBe 1
+    Par.run(pool)(unit(1)).get() shouldBe 1
   }
 
   it should "map2 two Pars" in {
-    Par.run(direct)(Par.map2(Par.unit(1), Par.unit(2))(_ + _)).get() shouldBe 3
-    Par.run(pool)(Par.map2(Par.unit(1), Par.unit(2))(_ + _)).get() shouldBe 3
+    Par.run(direct)(Par.map2(unit(1), unit(2))(_ + _)).get() shouldBe 3
+    Par.run(pool)(Par.map2(unit(1), unit(2))(_ + _)).get() shouldBe 3
   }
 
   it should "map2_1 two Pars" in {
-    Par.run(direct)(Par.map2_1(Par.unit(1), Par.unit(2))(_ + _)).get() shouldBe 3
-    Par.run(pool)(Par.map2_1(Par.unit(1), Par.unit(2))(_ + _)).get() shouldBe 3
+    Par.run(direct)(Par.map2_1(unit(1), unit(2))(_ + _)).get() shouldBe 3
+    Par.run(pool)(Par.map2_1(unit(1), unit(2))(_ + _)).get() shouldBe 3
   }
 
   it should "convert function into async function" in {
@@ -32,19 +33,19 @@ class ParTest extends AnyFlatSpec with Matchers {
   }
 
   it should "sort list in Par" in {
-    val lp = Par.unit(List(2,1,5,4,3))
+    val lp = unit(List(2,1,5,4,3))
     Par.run(direct)(Par.sortPar(lp)).get() shouldBe List(1,2,3,4,5)
     Par.run(pool)(Par.sortPar(lp)).get() shouldBe List(1,2,3,4,5)
   }
 
   it should "map list into sorted list" in {
-    val lp = Par.unit(List(2,1,5,4,3))
-    Par.run(direct)(Par.map(lp)(_.sorted)).get() shouldBe List(1,2,3,4,5)
-    Par.run(pool)(Par.map(lp)(_.sorted)).get() shouldBe List(1,2,3,4,5)
+    val lp = unit(List(2,1,5,4,3))
+    Par.run(direct)(map(lp)(_.sorted)).get() shouldBe List(1,2,3,4,5)
+    Par.run(pool)(map(lp)(_.sorted)).get() shouldBe List(1,2,3,4,5)
   }
 
   it should "map_1 list into sorted list" in {
-    val lp = Par.unit(List(2,1,5,4,3))
+    val lp = unit(List(2,1,5,4,3))
     Par.run(direct)(Par.map_1(lp)(_.sorted)).get() shouldBe List(1,2,3,4,5)
     Par.run(pool)(Par.map_1(lp)(_.sorted)).get() shouldBe List(1,2,3,4,5)
   }
@@ -69,34 +70,49 @@ class ParTest extends AnyFlatSpec with Matchers {
 
   it should "sequence List of Pars" in {
     Par.run(direct)(Par.sequence(List())).get() shouldBe List()
-    Par.run(direct)(Par.sequence(List(Par.unit(1)))).get() shouldBe List(1)
-    Par.run(direct)(Par.sequence(List(Par.unit(1), Par.unit(2), Par.unit(3)))).get() shouldBe List(1,2,3)
+    Par.run(direct)(Par.sequence(List(unit(1)))).get() shouldBe List(1)
+    Par.run(direct)(Par.sequence(List(unit(1), unit(2), unit(3)))).get() shouldBe List(1,2,3)
 
     Par.run(pool)(Par.sequence(List())).get() shouldBe List()
-    Par.run(pool)(Par.sequence(List(Par.unit(1)))).get() shouldBe List(1)
-    Par.run(pool)(Par.sequence(List(Par.unit(1), Par.unit(2), Par.unit(3)))).get() shouldBe List(1,2,3)
+    Par.run(pool)(Par.sequence(List(unit(1)))).get() shouldBe List(1)
+    Par.run(pool)(Par.sequence(List(unit(1), unit(2), unit(3)))).get() shouldBe List(1,2,3)
   }
 
   it should "create product of two Pars" in {
-    val pa = Par.unit(1)
-    val pb = Par.unit(2)
+    val pa = unit(1)
+    val pb = unit(2)
     Par.run(direct)(Par.product(pa, pb)).get shouldBe (1,2)
     Par.run(pool)(Par.product(pa, pb)).get shouldBe (1,2)
   }
 
   // Book's example of identity law: map(unit(1))(_ + 1) == unit(2)
-  "identity law" should "enforce equivalence of mapping over a unit with the resulting unit" in {
-    isEquivalent(direct)(Par.map(Par.unit(1))(_ + 1), Par.unit(2)) shouldBe true
+  "unit-map identity law" should "enforce equivalence of mapping over a unit with the resulting unit" in {
+    isEquivalent(direct)(map(unit(1))(_ + 1), unit(2)) shouldBe true
   }
 
   // Book's example of identity law: map(unit(x))(f) == unit(f(x))
   it should "hold for any choice of x and f" in {
     val f: Int => Int = _ + 1
     val x = 1
-    isEquivalent(direct)(Par.map(Par.unit(x))(f), Par.unit(f(x))) shouldBe true
+    isEquivalent(direct)(map(unit(x))(f), unit(f(x))) shouldBe true
   }
 
+  // Book's example
+  "map identity law" should "be defined in terms of simpler laws" in {
+    val f: Int => Int = _ + 1
+    val x = 1
+    val y = Par.async(2)
+
+    isEquivalent(direct)(map(unit(x))(f), unit(f(x))) shouldBe true
+    isEquivalent(direct)(map(unit(x))(id), unit(id(x))) shouldBe true
+    isEquivalent(direct)(map(unit(x))(id), unit(x)) shouldBe true
+    isEquivalent(direct)(map(y)(id), y) shouldBe true
+  }
+
+
   // Helpers
+  def id[A](a: A): A = a
+
   def isEquivalent[A](e: ExecutorService)(p: Par[A], p2: Par[A]): Boolean = p(e).get == p2(e).get
 
 }
